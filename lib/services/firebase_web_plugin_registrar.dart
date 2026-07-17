69defd7ebcb4098118e121b09ab33ae9dffe71a1
ali dandash <alimjdandash@gmail.com>
2026-07-17 10:24:20 +0300

    Use web condition for Firebase plugin registration

--- lib/services/firebase_web_plugin_registrar.dart
+++ lib/services/firebase_web_plugin_registrar.dart
@@ -1,2 +1,2 @@
 export 'firebase_web_plugin_registrar_stub.dart'
-    if (dart.library.js_interop) 'firebase_web_plugin_registrar_web.dart';
+    if (dart.library.html) 'firebase_web_plugin_registrar_web.dart';
